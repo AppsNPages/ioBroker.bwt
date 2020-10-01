c4f5e3e6267000dcef9cd387c29e57ed54ab4f54
Erik Haselsteiner <erik@haselsteiner.info>
2020-10-01 22:21:19 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"bwt adapter settings": {
+		"en": "Adapter settings for bwt",
+		"de": "Adaptereinstellungen für bwt",
+		"ru": "Настройки адаптера для bwt",
+		"pt": "Configurações do adaptador para bwt",
+		"nl": "Adapterinstellingen voor bwt",
+		"fr": "Paramètres d'adaptateur pour bwt",
+		"it": "Impostazioni dell'adattatore per bwt",
+		"es": "Ajustes del adaptador para bwt",
+		"pl": "Ustawienia adaptera dla bwt",
+		"zh-cn": "bwt的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
